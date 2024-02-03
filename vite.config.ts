54e647a1a71b6caaa9824f6f0d27574ac0dc037c
Tyler Mayoff <tyler@tylermayoff.com>
2024-02-03 15:04:45 -0500

    trying to get the manifest file to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,58 @@
-import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import builtins from "builtin-modules";
-import UnoCSS from "unocss/vite";
-import { PluginOption, defineConfig } from "vite";
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import builtins from 'builtin-modules';
+import UnoCSS from 'unocss/vite';
+import { PluginOption, defineConfig } from 'vite';
 
 const setOutDir = (mode: string) => {
-     switch (mode) {
-      case "development":
-         return "./test_vault/.obsidian/plugins/obsidian-meals";
-       case "production":
-         return "./build";
-     }
+    switch (mode) {
+        case 'development':
+            return './test_vault/.obsidian/plugins/obsidian-meals';
+        case 'production':
+            return './build';
+    }
 };
 
 export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      UnoCSS(),
-      svelte({
-        preprocess: vitePreprocess(),
-        compilerOptions: {
-          customElement: true,
-        },
-      }) as PluginOption,
-    ],
-    build: {
-      lib: {
-        entry: "src/main",
-        formats: ["cjs"],
-      },
-      rollupOptions: {
-        output: {
-          entryFileNames: "main.js",
-          assetFileNames: "styles.css",
-        },
-        external: [
-          "obsidian",
-          "electron",
-          "@codemirror/autocomplete",
-          "@codemirror/collab",
-          "@codemirror/commands",
-          "@codemirror/language",
-          "@codemirror/lint",
-          "@codemirror/search",
-          "@codemirror/state",
-          "@codemirror/view",
-          "@lezer/common",
-          "@lezer/highlight",
-          "@lezer/lr",
-          ...builtins,
+    return {
+        plugins: [
+            UnoCSS(),
+            svelte({
+                preprocess: vitePreprocess(),
+                compilerOptions: {
+                    customElement: true,
+                },
+            }) as PluginOption,
         ],
-      },
-      outDir: setOutDir(mode),
-      emptyOutDir: false,
-      sourcemap: "inline",
-    },
-  };
+        build: {
+            lib: {
+                entry: 'src/main',
+                formats: ['cjs'],
+            },
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'main.js',
+                    assetFileNames: 'styles.css',
+                },
+                external: [
+                    'obsidian',
+                    'electron',
+                    '@codemirror/autocomplete',
+                    '@codemirror/collab',
+                    '@codemirror/commands',
+                    '@codemirror/language',
+                    '@codemirror/lint',
+                    '@codemirror/search',
+                    '@codemirror/state',
+                    '@codemirror/view',
+                    '@lezer/common',
+                    '@lezer/highlight',
+                    '@lezer/lr',
+                    ...builtins,
+                ],
+            },
+            outDir: setOutDir(mode),
+            emptyOutDir: false,
+            sourcemap: mode === 'production' ? false : 'inline',
+        },
+    };
 });
