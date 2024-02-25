c282a55e557eb472fcfc6783eb5cbd7560e0143d
Tyler Mayoff <tyler@tylermayoff.com>
2024-02-25 15:26:13 -0500

    Fixed build

--- src/meal_plan/utils.ts
+++ src/meal_plan/utils.ts
@@ -1,4 +1,3 @@
-import { type TokensList } from 'marked';
 import moment from 'moment';
 
 export function get_current_week() {
@@ -8,13 +7,3 @@ export function get_current_week() {
     return moment(last_sunday_date).format('MMMM Do');
 }
 
-export function get_index_of_heading(lexed: TokensList, depth = 1, start = 0) {
-    for (let i = start; i < lexed.length; i++) {
-        const tok = lexed[i];
-        if (tok.type === 'heading' && tok.depth === depth) {
-            return i;
-        }
-    }
-
-    return -1;
-}
