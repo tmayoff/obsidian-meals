2d71f630847e053ad05680d957656bd8c917be94
Tyler Mayoff <tyler@tylermayoff.com>
2025-04-13 10:11:06 -0400

    remove warnings

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -2,6 +2,8 @@
 let { enabled = $bindable(false) } = $props();
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
   class={["checkbox-container", enabled ? "is-enabled" : ""]}
   onclick={() => {
