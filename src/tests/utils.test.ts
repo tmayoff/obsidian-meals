e2d41b5499aba891f4ef41b4d80ac69102863797
Tyler Mayoff <tyler@tylermayoff.com>
2025-04-13 00:26:19 -0400

    Adding tests to utils file

---
+++ src/tests/utils.test.ts
@@ -0,0 +1,56 @@
+import { Ok, type Result } from 'ts-results-es';
+import { expect, test } from 'vitest';
+import { ShoppingListIgnoreBehaviour } from '../settings/settings.ts';
+import { type BehaviourValidationError, validateIgnoreBehaviour, wildcardToRegex } from '../utils/utils.ts';
+
+test('wildcardToRegex', () => {
+    interface Test {
+        input: string;
+        expected: RegExp;
+    }
+
+    // biome-ignore lint/performance/useTopLevelRegex: <explanation>
+    const tests: Test[] = [{ input: '*', expected: /^.*$/ }];
+
+    for (const test of tests) {
+        const actual = wildcardToRegex(test.input);
+
+        expect(actual).toStrictEqual(test.expected);
+    }
+});
+
+test('validateIgnoreBehaviour_ExactPartial', () => {
+    const input = ['salt', 'pepper'];
+
+    let res = validateIgnoreBehaviour(input, ShoppingListIgnoreBehaviour.Exact);
+
+    expect(res.isOk());
+
+    res = validateIgnoreBehaviour(input, ShoppingListIgnoreBehaviour.Exact);
+
+    expect(res.isOk());
+});
+
+test('validateIgnoreBehaviour_Regex', () => {
+    interface Test {
+        input: string[];
+        expected: Result<boolean, BehaviourValidationError>;
+    }
+
+    const tests: Test[] = [
+        {
+            input: ['salt.*', 'pepper'],
+            expected: Ok(true),
+        },
+        {
+            // TODO(tyler) Failing test here
+            input: ['salt'],
+            expected: Ok(true),
+        },
+    ];
+
+    for (const test of tests) {
+        const actual = validateIgnoreBehaviour(test.input, ShoppingListIgnoreBehaviour.Exact);
+        expect(actual).toStrictEqual(test.expected);
+    }
+});
